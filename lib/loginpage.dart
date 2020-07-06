38bec6da55ddd6f7c1cbcc06836f9a0f3eda9f88
pratik037 <singhalpratik037@gmail.com>
2020-07-07 00:46:41 +0530

    Created alternate design for the app with logos and color schemes

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:tesseractmeetup/authService.dart';
 import 'package:tesseractmeetup/registerpage.dart';
@@ -13,83 +14,127 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _password = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   final _key = GlobalKey<ScaffoldState>();
+  TextStyle style = TextStyle(color: Colors.white, fontSize: 20);
 
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<AuthenticationService>(context);
     return Scaffold(
+      backgroundColor: Color(0xff6C63FF),
       key: _key,
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text('Login', style: TextStyle(fontSize: 30),),
+        backgroundColor: Color(0xff6C63FF),
+        elevation: 0,
+        centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: Center(
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _email,
-                  validator: (value) =>
-                      value.isEmpty ? "Email cannot be empty" : null,
-                  decoration: InputDecoration(
-                      labelText: 'Email',
-                      prefixIcon: Icon(Icons.email),
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _password,
-                  obscureText: true,
-                  validator: (value) =>
-                      value.isEmpty ? "Password cannot be empty" : null,
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(Icons.vpn_key),
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              user.status == Status.Authenticating
-                  ? Center(child: CircularProgressIndicator())
-                  : Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: Material(
-                        elevation: 5,
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.red,
-                        child: MaterialButton(
-                          onPressed: () async {
-                            if (_formKey.currentState.validate()) {
-                              if (!await user.signIn(
-                                  _email.text, _password.text)) {
-                                _key.currentState.showSnackBar(SnackBar(
-                                    content:
-                                        Text("Verify the credentials again")));
-                              }
-                            }
-                          }, //Onpress ends here
-                          child: Text(
-                            "Sign In",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
+      body: ListView(
+              children: [
+                SvgPicture.asset('assets/images/login.svg', height: 200,),
+          Form(
+
+            key: _formKey,
+            child: Center(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: _email,
+                      validator: (value) =>
+                          value.isEmpty ? "Email cannot be empty" : null,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          gapPadding: 30
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          borderSide: BorderSide(
+                            color: Colors.black
+                          )
+                        ),
+                          labelText: 'Email',
+                          labelStyle: style,
+                          prefixIcon: Icon(Icons.email, color: Colors.white,),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30))
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: _password,
+                      obscureText: true,
+                      validator: (value) =>
+                          value.isEmpty ? "Password cannot be empty" : null,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          gapPadding: 30
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          gapPadding: 30
+                        ),
+                          labelText: 'Password',
+                          labelStyle: style,
+                          prefixIcon: Icon(Icons.vpn_key, color: Colors.white,),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30))
+                          )),
+                    ),
+                  ),
+                  user.status == Status.Authenticating
+                      ? Center(child: CircularProgressIndicator())
+                      : Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: Material(
+                            elevation: 5,
+                            borderRadius: BorderRadius.circular(30),
+                            color: Color(0xffffb663),
+                            child: MaterialButton(
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  if (!await user.signIn(
+                                      _email.text, _password.text)) {
+                                    _key.currentState.showSnackBar(SnackBar(
+                                        content:
+                                            Text("Verify the credentials again")));
+                                  }
+                                }
+                              }, //Onpress ends here
+                              child: Text(
+                                "Sign In",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
 
-                    FlatButton(onPressed: (){
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => RegisterPage())
-                      );
-                    }, child: Text("Register"))
-            ],
+                        FlatButton(onPressed: (){
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) => RegisterPage())
+                          );
+                        }, child: Text("Register", style: TextStyle(color: Colors.white),))
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/registerpage.dart
+++ lib/registerpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:tesseractmeetup/authService.dart';
 import 'package:tesseractmeetup/loginpage.dart';
@@ -20,100 +21,146 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     final user = Provider.of<AuthenticationService>(context);
     return Scaffold(
+      backgroundColor: Color(0xff6C63FF),
       key: _key,
       appBar: AppBar(
-        title: Text("Register"),
+        title: Text(
+          "Register",
+          style: TextStyle(fontSize: 30),
+        ),
+        backgroundColor: Color(0xff6C63FF),
+        elevation: 0,
+        centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: Center(
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _email,
-                  validator: (value) =>
-                      value.isEmpty ? "Email cannot be empty" : null,
-                  decoration: InputDecoration(
-                      labelText: 'Email',
-                      prefixIcon: Icon(Icons.email),
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _password,
-                  obscureText: true,
-                  validator: (value) =>
-                      value.isEmpty ? "Password cannot be empty" : null,
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(Icons.vpn_key),
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  controller: _retype,
-                  obscureText: true,
-                  validator: (value) => value.isEmpty
-                      ? "This cannot be empty"
-                      : value == _password.text
-                          ? null
-                          : "Both passwords do not match",
-                  decoration: InputDecoration(
-                      labelText: 'Confirm Password',
-                      prefixIcon: Icon(Icons.vpn_key),
-                      border: OutlineInputBorder()),
-                ),
-              ),
-              user.status == Status.Authenticating
-                  ? Center(child: CircularProgressIndicator())
-                  : Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: Material(
-                        elevation: 5,
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.red,
-                        child: MaterialButton(
-                          onPressed: () async {
-                            if (_formKey.currentState.validate()) {
-                              if (!await user.singUp(
-                                  _email.text, _password.text)) {
-                                _key.currentState.showSnackBar(
-                                  SnackBar(
-                                    content: Text(
-                                        "Failed to register, please try again"),
-                                  ),
-                                );
-                              }
-                              else{
-                                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
-                              }
-                            }
-                          }, //Onpress ends here
-                          child: Text(
-                            "Sign Up",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
+      body: ListView(
+        padding: EdgeInsets.only(top: 32),
+        children: [
+          SvgPicture.asset(
+            'assets/images/signup.svg',
+            height: 200,
+          ),
+          Form(
+            key: _formKey,
+            child: Center(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: _email,
+                      validator: (value) =>
+                          value.isEmpty ? "Email cannot be empty" : null,
+                      decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          labelText: 'Email',
+                          labelStyle: TextStyle(color: Colors.white),
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.white,
+                          ),
+                          border: OutlineInputBorder()),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: _password,
+                      obscureText: true,
+                      validator: (value) =>
+                          value.isEmpty ? "Password cannot be empty" : null,
+                      decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.white),
+                          prefixIcon: Icon(
+                            Icons.vpn_key,
+                            color: Colors.white,
+                          ),
+                          border: OutlineInputBorder()),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      controller: _retype,
+                      obscureText: true,
+                      validator: (value) => value.isEmpty
+                          ? "This cannot be empty"
+                          : value == _password.text
+                              ? null
+                              : "Both passwords do not match",
+                      decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          labelStyle: TextStyle(color: Colors.white),
+                          labelText: 'Confirm Password',
+                          prefixIcon: Icon(
+                            Icons.vpn_key,
+                            color: Colors.white,
+                          ),
+                          border: OutlineInputBorder()),
+                    ),
+                  ),
+                  user.status == Status.Authenticating
+                      ? Center(child: CircularProgressIndicator())
+                      : Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: Material(
+                            elevation: 5,
+                            borderRadius: BorderRadius.circular(30),
+                            color: Color(0xffffb663),
+                            child: MaterialButton(
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  if (!await user.singUp(
+                                      _email.text, _password.text)) {
+                                    _key.currentState.showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                            "Failed to register, please try again"),
+                                      ),
+                                    );
+                                  } else {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => HomePage()));
+                                  }
+                                }
+                              }, //Onpress ends here
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    child: Text(
+                      "Existing User? Login",
+                      style: TextStyle(color: Colors.white),
                     ),
-              FlatButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => LoginPage()));
-                  },
-                  child: Text("Login"))
-            ],
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
